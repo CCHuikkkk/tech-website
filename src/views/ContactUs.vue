b2f6ff6d7549c85fa9664a39d1abdc2e515d8725
CCHuikkk <1205831276@qq.com>
2025-02-22 22:14:11 +0800

    Update files and deploy to GitHub Pages

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,4 +1,5 @@
 <template>
+  <img src="../assets/1.jpg" alt="关于我们" class="banner-image" />
     <div class="content">
       <h2>联系我们</h2>
       <p>这里是联系我们的页面。</p>
@@ -12,6 +13,11 @@
   </script>
   
   <style scoped>
+   .banner-image {
+    width: 100%; /* 图片宽度跟随设备宽度 */
+    height: auto; /* 保持图片比例 */
+    display: block; /* 确保图片独占一行 */
+  }
   .content {
     padding: 20px;
     text-align: center;
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,6 +3,7 @@
         <h2>首页</h2>
         <p>欢迎来到科创首页！</p>
     </div>
+    <img src="../assets/3.jpg" alt="关于我们" class="banner-image" />
 </template>
   
 <script>
@@ -12,6 +13,11 @@
 </script>
 
 <style scoped>
+   .banner-image {
+    width: 100%; /* 图片宽度跟随设备宽度 */
+    height: auto; /* 保持图片比例 */
+    display: block; /* 确保图片独占一行 */
+  }
 .content {
   padding: 20px;
   text-align: center;
